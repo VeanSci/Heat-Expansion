c92e7069109af640be851aad0a98d8a015514a55
VeanSci <a31873201@gmail.com>
2026-05-26 00:17:25 +0900

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import ThermalExpansionApp from "./ThermalExpansion.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <ThermalExpansionApp />
-  </React.StrictMode>
-);
