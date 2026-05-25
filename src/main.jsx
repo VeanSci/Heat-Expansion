786cb9f4c06a60c151603787fd41081943f7ab2c
VeanSci <a31873201@gmail.com>
2026-05-26 00:08:27 +0900

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ThermalExpansionApp from "./ThermalExpansion.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ThermalExpansionApp />
+  </React.StrictMode>
+);
